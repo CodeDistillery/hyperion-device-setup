62b488e8978b9c02841ad49c135108f8adacd019
Lauri Junkkari <latenssi@gmail.com>
2020-10-27 14:16:26 +0200

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+1. `curl https://raw.githubusercontent.com/CodeDistillery/hyperion-device-setup/master/setup.sh -o setup.sh && chmod +x setup.sh`
+2. `curl https://raw.githubusercontent.com/CodeDistillery/hyperion-device-setup/master/.env -o .env`
+3. Configure `.env` file
+4. Configure variables in `setup.sh`
+5. `bash setup.sh`
--- setup.sh
+++ setup.sh
@@ -1,7 +1,47 @@
 ##### Set the variables below #####
 
-test=1234
+# Name of this device
+hostname=
+
+# Local user
+user=
+
+# Local login password, used with reverse ssh tunnel for example
+password=
 
 ###################################
 
-echo $test
+echo Running setup...
+
+sudo apt update && sudo apt upgrade -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install -y ufw libudev-dev libgphoto2-dev nodejs git
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+
+echo "Adding user $user..."
+sudo $hostname # Temporary
+sudo adduser --disabled-password --gecos "" $user
+sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $user
+echo "$user:$password" | sudo chpasswd
+sudo -H -u $user bash -c 'ssh-keygen -t rsa -b 4096 -N "" -C "$USER@$HOSTNAME" -f /home/$USER/.ssh/id_rsa'
+sudo -H -u $user bash -c 'cat /home/$USER/.ssh/id_rsa.pub'
+read -p "Copy key above and add to Github repo deploy keys. Press enter to continue.."
+
+echo "Cloning repo..."
+sudo -H -u $user bash -c 'git clone git@github.com:CodeDistillery/hyperion-device-node.git /home/$USER/app'
+
+echo "Installing project dependencies..."
+sudo cp .env /home/$user/app/ && sudo chown $user:$user /home/$user/app/.env
+sudo npm install -g pm2
+sudo -H -u $user bash -c 'cd /home/$USER/app && npm install'
+
+echo "Starting up the project..."
+sudo -H -u $user bash -c 'cd /home/$USER/app && pm2 start && pm2 startup'
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $user --hp /home/$user
+sudo -H -u $user bash -c 'cd /home/$USER/app && pm2 save'
+
+# echo "Deleting default user..."
+sudo deluser -remove-home pi
+
+# Requires reboot:
+sudo ufw limit OpenSSH && sudo ufw enable
