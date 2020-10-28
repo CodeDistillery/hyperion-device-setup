92e4293aaf0be79e2f97c41b308ce71654f4507c
Lauri Junkkari <latenssi@gmail.com>
2020-10-28 12:31:29 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -35,3 +35,6 @@ echo "Starting up the project..."
 sudo -H -u $user bash -c 'cd /home/$USER/app && pm2 start && pm2 startup'
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $user --hp /home/$user
 sudo -H -u $user bash -c 'cd /home/$USER/app && pm2 save'
+
+echo "Cleaning up..."
+rm setup.sh && rm local.json
